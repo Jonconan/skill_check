17e7b6580546ace7d32bd17490c15166f6c123d1
Jonconan <jun.miyahara.1020@gmail.com>
2021-12-16 01:51:31 +0900

    動作検証用　あったら便利ページ

--- 6_0_0/ch14/config/routes.rb
+++ 6_0_0/ch14/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users do
     member do
-      get :following, :followers
+      get :following, :followers, :notices
     end
   end
   resources :users
